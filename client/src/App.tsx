0b48b03f1737e1b1a1a4f86ab7deb846826ef707
Adithya-Ekanayake <22fis0550@ms.sab.ac.lk>
2026-08-27 01:13:06 +0530

    Add reports page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ import Transactions from "./pages/Transactions";
 import Analytics from "./pages/Analytics";
 import Reports from "./pages/Reports";
 
+
 function App() {
   return (
     <BrowserRouter>
